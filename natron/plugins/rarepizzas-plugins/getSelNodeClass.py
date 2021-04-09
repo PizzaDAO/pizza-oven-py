d44671c20affbfda5b0560d6184cb9eb4343927b
Anthony Shafer <anthonyshafer@gmail.com>
2021-04-09 14:57:26 -0700

    python working to hand data

--- natron/plugins/rarepizzas-plugins/getSelNodeClass.py
+++ natron/plugins/rarepizzas-plugins/getSelNodeClass.py
@@ -19,3 +19,14 @@ def getSelNodesClass():
         os.write(1,'\n' + str(currentID) + '\n')
         print(str(currentID) + '\n')
 
+import os
+import json
+ko = "/Users/anthonyshafer/Dropbox/RarePizzas/rarepizzas-repo/pizza-oven-py/kitchen_order.json"
+myfile = file(ko)
+data = myfile.read()
+print dir(data)
+# parse file
+obj = json.loads(data)
+
+# show values
+print obj["order"].keys()
---
+++ natron/waxpaper.py
@@ -0,0 +1,26 @@
+# This is the content of a natron rarepizzas script
+# Anthony Shafer aka "Shrimp" @anthonyshafer
+
+# files
+filein = "0500-waxpaper-redchecker.png"
+fileout = "rp-#####-waxpaper.png"
+
+# paths
+oven = "../oven/"
+idb = "../ingredients-db/"
+
+# get ingredient from kitchen
+lastNode = app.getNode("i1")
+param = lastNode.getParam("filename")
+if param is not None:
+	param.setValue(idb+filein)
+del param
+
+# do some otherstuff...
+
+# bake to oven
+lastNode= app.getNode("w1")
+param = lastNode.getParam("filename")
+if param is not None:
+	param.setValue(oven+fileout)
+del param
