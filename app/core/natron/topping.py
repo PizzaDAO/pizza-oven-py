c5eaa9d561aa34d2f86c56015c2616fd0c08307b
Matt Wilhelm <github@addressxception.com>
2021-08-13 21:01:17 -0400

    missed the file lol

--- app/core/natron/topping.py
+++ app/core/natron/topping.py
@@ -4,7 +4,27 @@ from natron_base import NatronBase
 
 
 class Topping(NatronBase):
-    """base class override"""
+    """base class override handles single input only for now"""
+
+    MAX_INSTANCES = 30
+
+    def __init__(self, app, env_var):
+        self.natron = app
+        self._load_json_data(env_var)
+
+        # TODO: allow for loading one file per input node
+        self._load_image(self.in_file, "input_single")
+
+    def render(self):
+        """override the render function in your derived class to customize behavior"""
+        self._disableOthers()
+        self.enable("single")
+        self.setTransform(self.translate, self.scale, self.rotate)
+        self.setOutput()
+
+    def _disableOthers(self):
+        for i in range(0, self.MAX_INSTANCES):
+            self.disable(i)
 
 
 instance = Topping(app, "TOPPING_DATA_PATH")
