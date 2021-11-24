7129ea073c637973c74d4ac520a29c994ea5c4a0
Matt Wilhelm <github@addressxception.com>
2021-11-24 14:48:31 -0500

    test re run workflow

--- app/core/order_task.py
+++ app/core/order_task.py
@@ -70,10 +70,12 @@ def publish_order_result(
     print(f"{render_task.job_id} - decoded_metadata: {decoded_metadata.hex()}")
     print(f"{render_task.job_id} - truncated_metadata: {truncated_metadata}")
     print(f"{render_task.job_id} - truncated_metadata_len: {len(truncated_metadata)}")
-    print("\n --------- YOUR PIZZA IS COMPLETE ---------- \n")
-    print(f"\n    ipfs image: {metadata.image} \n")
-    print(f"\n    ipfs crust_alpha: {image_alpha_hash} \n")
-    print("\n ------------------------------------------- \n")
+    print("\n --------------- YOUR PIZZA IS COMPLETE ---------------- \n")
+    print(f"\n    ipfs metadata:       ipfs://{metadata.image} \n")
+    print(f"\n    ipfs image:          ipfs://{metadata.image} \n")
+    print(f"\n    ipfs crust_alpha:    ipfs://{image_alpha_hash} \n")
+    print(f"\n    tokenURI string:     {from_bytes_big} \n")
+    print("\n ------------------------------------------------------- \n")
 
     # build the response object
     response = OrderPizzaResponse(
